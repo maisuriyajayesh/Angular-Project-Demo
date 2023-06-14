2c66bf5e67169e305b003f6f2870293cafce37e5
Jayesh Maisuriya <jayesh.maisuriya@i-link.co.in>
2023-06-14 14:50:48 +0530

    todolist

--- src/app/to-do-list/to-do-list.component.ts
+++ src/app/to-do-list/to-do-list.component.ts
@@ -6,20 +6,18 @@ import { Component, OnInit } from '@angular/core';
   templateUrl: './to-do-list.component.html',
   styleUrls: ['./to-do-list.component.scss']
 })
-export class ToDoListComponent   implements OnInit{
-  show:boolean = false;
-  buttonName:any = 'Show';
-  Name:any;
-  myDropdown:any;
-  studentArr: Array<any> = []; 
+export class ToDoListComponent implements OnInit {
+
+  show: boolean = false;
+  buttonName: any = 'Show';
+  Name: any;
+  myDropdown: any;
+  studentArr: Array<any> = [];
   student: any = {
     studentId: 0,
     Name: '',
   }
- 
-   
-
-  onAdd(userForm:any){
+  onAdd(userForm: any) {
     this.student.studentId = this.studentArr.length + 1;
     this.studentArr.push(this.student);
     localStorage.setItem("studentlist", JSON.stringify(this.studentArr));
@@ -28,18 +26,13 @@ export class ToDoListComponent   implements OnInit{
       Name: ''
     }
   }
+  onEdit(user:any){
+
+  }
   ngOnInit(): void {
     const localData = localStorage.getItem('studentlist');
     if (localData != null) {
       this.studentArr = JSON.parse(localData);
-    }
-    const myDropdown = document.getElementById('myDropdown');
-    if (myDropdown) {
-      myDropdown.addEventListener('show.bs.dropdown', () => {
-        // do something...
-        console.log('Dropdown is being shown');
-        // Perform additional actions as needed
-      });
     }
   }
 
